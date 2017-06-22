f03c8118ec6c768c53984b392eeaa5772d638af7
Bernd Weber <bernd@nvisionary.com>
2017-06-21 21:20:57 -0700

    BT23 Slight refactoring and adding rotators.

--- Source/Section_04/Private/TankAimingComponent.cpp
+++ Source/Section_04/Private/TankAimingComponent.cpp
@@ -14,21 +14,7 @@ UTankAimingComponent::UTankAimingComponent()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
-}
-
-
-// Called when the game starts
-void UTankAimingComponent::BeginPlay()
-{
-	Super::BeginPlay();
-}
-
-
-// Called every frame
-void UTankAimingComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
-{
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	PrimaryComponentTick.bCanEverTick = false;
 }
 
 void UTankAimingComponent::AimAt (FVector HitLocation, float LaunchSpeed)
@@ -52,12 +38,26 @@ void UTankAimingComponent::AimAt (FVector HitLocation, float LaunchSpeed)
 		)
 	{
 		auto AimDirection = LaunchVelocity.GetSafeNormal ();
-		auto TankName = GetOwner ()->GetName ();
-		UE_LOG (LogTemp, Warning, TEXT ("%s aiming at %s"), *TankName, *(AimDirection.ToString ()));
+		MoveBarrelTowards (AimDirection);
 	}
+	// No target solution; do nothing
 }
 
 void UTankAimingComponent::SetBarrelReference (UStaticMeshComponent* BarrelToSet)
 {
 	Barrel = BarrelToSet;
+}
+
+void UTankAimingComponent::MoveBarrelTowards (FVector& AimDirection)
+{
+	// Work out difference between current barrel location and aim direction
+	auto BarrelRotator = Barrel->GetForwardVector ().Rotation ();
+	auto AimAsARotator = AimDirection.Rotation ();
+	auto DeltaRotator = AimAsARotator - BarrelRotator;
+
+	UE_LOG (LogTemp, Warning, TEXT ("AimAsRotator: %s"), *(AimAsARotator.ToString ()));
+
+	// Move the barell the right amount this frame 
+
+	// Give a max elevation speed and the frame time
 }
--- Source/Section_04/Public/TankAimingComponent.h
+++ Source/Section_04/Public/TankAimingComponent.h
@@ -16,19 +16,15 @@ public:
 	// Sets default values for this component's properties
 	UTankAimingComponent();
 
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
-
 public:
 	void AimAt (FVector HitLocation, float LaunchSpeed);
 
 	void SetBarrelReference (UStaticMeshComponent* BarrelToSet);
 
+	// TODO Add SetTurretReference()
+
 private:
 	UStaticMeshComponent* Barrel = nullptr;
+
+	void MoveBarrelTowards (FVector& AimDirection);
 };
