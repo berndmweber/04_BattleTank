f65dc20f454af8dbe48993dee611d77888e8bd23
Bernd Weber <bernd@nvisionary.com>
2017-06-24 10:14:36 -0700

    BT38 Applying force to the tank

--- Source/Section_04/Public/TankTrack.h
+++ Source/Section_04/Public/TankTrack.h
@@ -17,4 +17,9 @@ class SECTION_04_API UTankTrack : public UStaticMeshComponent
 public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle (float Throttle);
+
+private:
+	// This is max force per track in Newtons;
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000.0f; // 40tons * 10m/s^2 (1g)
 };
