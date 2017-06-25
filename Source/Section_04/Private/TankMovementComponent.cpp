41504e37259895ee0d9b153de29bfdae3b41be33
Bernd Weber <bernd@nvisionary.com>
2017-06-24 18:27:46 -0700

    BT40 Can now also move left/right

--- Source/Section_04/Private/TankMovementComponent.cpp
+++ Source/Section_04/Private/TankMovementComponent.cpp
@@ -5,7 +5,6 @@
 
 void UTankMovementComponent::Initialize (UTankTrack* LeftTrackToSet, UTankTrack* RightTrackToSet)
 {
-	if (!LeftTrackToSet || !RightTrackToSet) { return; }
 	LeftTrack = LeftTrackToSet;
 	RightTrack = RightTrackToSet;
 }
@@ -13,8 +12,15 @@ void UTankMovementComponent::Initialize (UTankTrack* LeftTrackToSet, UTankTrack*
 void UTankMovementComponent::IntendMoveForward (float Throw)
 {
 	if (!LeftTrack || !RightTrack) { return; }
-	UE_LOG (LogTemp, Warning, TEXT ("Intend move forward throw: %f"), Throw);
 	LeftTrack->SetThrottle (Throw);
+	RightTrack->SetThrottle (Throw);
+	// TODO prevent double speed due to double input
+}
+
+void UTankMovementComponent::IntendTurnRight (float Throw)
+{
+	if (!LeftTrack || !RightTrack) { return; }
+	LeftTrack->SetThrottle (-Throw);
 	RightTrack->SetThrottle (Throw);
 	// TODO prevent double speed due to double input
 }
